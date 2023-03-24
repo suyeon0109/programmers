0bdfdc39377f4a6e9e9d8bc4e0a341cacdbe3b66
suyeon_minds <caelum0109@mindslab.ai>
2023-03-24 23:58:30 +0900

    23.03.24

--- test.py
+++ test.py
@@ -1,11 +1,32 @@
-def solution(s):
-    s = s.lstrip('{{')
-    s = s.rstrip('}}')
-    lst = s.split('},{')
-    for i in range(len(lst)):
-        lst[i] = lst[i].split(',')
-    print(lst)
-    answer = []
-    return answer
+from collections import deque
+from itertools import combinations
 
-solution("{{2},{2,1},{2,1,3},{2,1,3,4}}")
+def solution(elements):
+
+    lst = deque()
+
+    len_el = len(elements)
+    length = 1
+
+    for i in combinations(elements,length):
+        print(i)
+        print(sum(i))
+        lst.append(sum(i))
+    print(lst)
+    length += 1
+
+
+    # while length <= len_el:
+
+    #     for k in range(len_el): 
+    #         ssum = 0
+    #         for i in range(k, k + length):
+    #             ssum += elements[i%len_el]
+    #         lst.append(ssum)
+
+    #     length += 1
+
+    lst = list(set(lst))
+    return len(lst)
+
+print(solution([7,9,1,1,4]))
